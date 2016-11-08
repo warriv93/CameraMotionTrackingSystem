5bb5b9f42bf918cfd1f8f0026bd0814ad21791a7
warriv93 <warriv93@gmail.com>
2016-11-08 13:52:45 +0100

    expanded ajax function

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -32,30 +32,30 @@ app.use('/motion', motion);
 //DATABASE
 
 var mongoose = require('mongoose');
-
-var mongoURI = "mongodb://localhost/test2";
-var db = mongoose.connect(mongoURI, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Finally connected!!!');
-
-        //Create a schema for Book
-        var bookSchema = mongoose.Schema({
-            name: String,
-            //Also creating index on field isbn
-            isbn: {type: String, index: true},
-            author: String,
-            pages: Number
-        });
-        //Create a Model by using the schema defined above
-        //Optionally one can provide the name of collection where the instances
-        //of this model get stored. In this case it is "mongoose_demo". Skipping
-        //this value defaults the name of the collection to plural of model name i.e books.
-        var Book = mongoose.model('Book', bookSchema);
-        console.log('DB done');
-    }
-});
+//
+// var mongoURI = "mongodb://localhost/test2";
+// var db = mongoose.connect(mongoURI, function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('Finally connected!!!');
+//
+//         //Create a schema for Book
+//         var bookSchema = mongoose.Schema({
+//             name: String,
+//             //Also creating index on field isbn
+//             isbn: {type: String, index: true},
+//             author: String,
+//             pages: Number
+//         });
+//         //Create a Model by using the schema defined above
+//         //Optionally one can provide the name of collection where the instances
+//         //of this model get stored. In this case it is "mongoose_demo". Skipping
+//         //this value defaults the name of the collection to plural of model name i.e books.
+//         var Book = mongoose.model('Book', bookSchema);
+//         console.log('DB done');
+//     }
+// });
 
 //Get all the books
 app.get('/book', function (req, res) {
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-
 /* GET home page. */
 router.get('/', function(request, response, next) {
 
-  response.render('index', { title: 'hhhhhhh' });
+  response.render('index', { title: 'Motion camera timeline' });
 
   request.on('data', function (chunk) {
-    console.log('GOT DATA!');
+      console.log('GOT DATA!');
 
   });
-
-
-
-
 });
 
 module.exports = router;
